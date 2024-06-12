17cb0a4d0f3fb0fcca1b7dc68aa5545d76103551
sco3 <scorta@gmail.com>
2024-06-12 15:57:51 +0100

    go works and impressive

--- src/main/go/fastjson_main.go
+++ src/main/go/fastjson_main.go
@@ -8,20 +8,37 @@ import (
 	"github.com/valyala/fastjson"
 )
 
-func main() {
-	start := time.Now()
+func run() {
 	home, err := os.UserHomeDir()
 	if err == nil {
 		b, err := os.ReadFile(home + "/.local/devices.json")
 		if err == nil {
 			str := string(b)
-			fmt.Println(str)
+			//fmt.Println(str)
 			var p fastjson.Parser
 			v, err := p.Parse(str)
 			if err == nil {
-				fmt.Printf("returnCode: %v\n", v.GetInt("returnCode"))
+				const field = "returnCode"
+				fmt.Printf("%s: %v\n", field, v.GetInt(field))
+				v.Set(field, fastjson.MustParse("1"))
+				mod := v.String()
+				out := "/.local/devices-out-go.json"
+				err = os.WriteFile(home+out, []byte(mod), 0644)
+				if err != nil {
+					os.Exit(1)
+				}
 			}
 		}
 	}
-	fmt.Printf("Time: %d ms\n", time.Since(start)/time.Millisecond)
+}
+
+func main() {
+	start := time.Now()
+	n := int64(1000)
+	for i := int64(0); i < n; i++ {
+		run()
+	}
+	took := time.Since(start).Milliseconds()
+	avg := float64(took) / float64(n)
+	fmt.Printf("Time: %d ms avg: %v runs: %d\n", took, avg, n)
 }
