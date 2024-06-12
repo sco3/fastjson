cb6241631b763f4ac613c53d5ef23dbfe117c686
Dmitry Zakharov <zakharov@ie.ibm.com>
2024-06-12 02:29:26 +0100

    does not work check later

--- src/main/rust/main.rs
+++ src/main/rust/main.rs
@@ -2,11 +2,14 @@ use dirs;
 use serde_json::from_str;
 use serde_json::Value;
 use std::fs::read_to_string;
+use std::fs::File;
+use std::io::Write;
 use std::time::Instant;
 
-fn get_int(v: Value, s: &str) -> i64 {
+fn _get_int(v: Value, s: &str) -> i64 {
     return v[s].as_i64().unwrap_or(0);
 }
+
 fn run() {
     let file_path = dirs::home_dir() //
         .unwrap()
@@ -15,8 +18,22 @@ fn run() {
     let s = read_to_string(file_path) //
         .unwrap_or_default();
 
-    let json: Value = from_str(&s).unwrap();
-    let _v = get_int(json, "returnCode");
+    let mut json: Value = from_str(&s).unwrap_or_default();
+    //let _v = get_int(json, "returnCode");
+
+    if let Some(rc) = json.get_mut("returnCode") {
+        *rc = Value::from(1);
+    }
+
+    let mut file = File::create(
+        dirs::home_dir() //
+            .unwrap()
+            .join(".local/devices-out.json"),
+    );
+    match file {
+        Ok(file) => file.write_all(json.as_bytes()),
+        Err(e) => Ok(),
+    }
 
     //println!("{}", v);
 }
