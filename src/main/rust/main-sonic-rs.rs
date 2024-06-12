a4d78169f5fb09b26a66c4d4a4f9941215fa8209
sco3 <scorta@gmail.com>
2024-06-12 12:31:44 +0100

    works better

--- src/main/rust/main-sonic-rs.rs
+++ src/main/rust/main-sonic-rs.rs
@@ -2,18 +2,13 @@ use sonic_rs::{from_str, JsonValueTrait, Value};
 use std::fs::read_to_string;
 
 fn main() {
-    let json = r#"{
-        "returnCode": 18
-    }"#;
-
     let file_path = dirs::home_dir() //
         .unwrap()
         .join(".local/devices.json");
 
-    let s = read_to_string(file_path) //
-        .unwrap_or_default();
+    let s = read_to_string(file_path).unwrap_or_default();
 
-    let root: Value = from_str(json).unwrap();
+    let root: Value = from_str(&*s).unwrap();
 
     let v = root.get("returnCode").as_i64().unwrap_or_default();
     println!("v: {}", v);
